3b1360c4b94fc7fbc85497e8ec3598ad3e6c98ac
3axap <uvanov977777@gmail.com>
2022-06-09 22:11:11 +0300

    add to component property

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,12 +2,19 @@ import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
 import EmployersList from '../employers-list/employers-list';
-import EmployeesAddForm from '../employers-add-form/employers-add-form';
+import EmployersAddForm from '../employers-add-form/employers-add-form';
 
 import './app.css';
 
 
 function App(){
+
+    const data = [
+        {name:'Jonh C.', salary: '800', increase: true, id: 1},
+        {name:'Alex M.', salary: '3000', increase: false, id: 2},
+        {name:'Carl W.', salary: '15000', increase: false, id: 3},
+    ];
+
     return(
         <div className="app">
             <AppInfo/>
@@ -17,8 +24,8 @@ function App(){
                 <AppFilter/>
             </div>
 
-            <EmployersList/>
-            <EmployeesAddForm/>
+            <EmployersList data={data}/>
+            <EmployersAddForm/>
         </div>
     )
 }
--- src/components/employers-list/employers-list.js
+++ src/components/employers-list/employers-list.js
@@ -1,13 +1,20 @@
-import EmployeesListItem from "../employers-list-item/employers-list-item"
+import EmployersListItem from "../employers-list-item/employers-list-item"
 import './employers-list.css'
 
 
-const EmployersList = () => {
+const EmployersList = ({data}) => {
+
+    const elements = data.map(item => {
+        const {id, ...itemProps} = item;
+        return (
+            <EmployersListItem key={id} {...itemProps}/>
+            )
+    })
+
     return(
         <ul className="app-list list-group">
-            <EmployeesListItem/>
-            <EmployeesListItem/>
-            <EmployeesListItem/>
+
+           {elements}
         </ul>
     )
 }
